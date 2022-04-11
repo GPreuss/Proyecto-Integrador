fa753b43d678df85cfd08278a115bf888b7f60b8
GPreuss <gpreuss@udesa.edu.ar>
2022-04-11 15:47:48 -0300

    rutas

--- proyectoIntegrador/app.js
+++ proyectoIntegrador/app.js
@@ -6,6 +6,13 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var loginRouter = require('./routes/login');
+var productAddRouter = require('./routes/product-add');
+var productRouter = require('./routes/product');
+var profileEditRouter = require('./routes/profile-edit');
+var profileRouter = require('./routes/profile');
+var registerRouter = require('./routes/register');
+var searchResultsRouter = require('./routes/search-results');
 
 var app = express();
 
@@ -21,14 +28,13 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-/*app.use('/login', loginRouter);
+app.use('/login', loginRouter);
 app.use('/product', productRouter);
 app.use('/product-add', productAddRouter);
 app.use('profile', profileRouter);
 app.use('/register', registerRouter);
 app.use('/profile-edit', profileEditRouter);
-app.use('/search-results', searchResultsRouter);*/
-
+app.use('/search-results', searchResultsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ proyectoIntegrador/db/data.js
@@ -0,0 +1,21 @@
+let data = {
+    usuario: {
+        nombre: "",
+        DNI: "",
+        email: "",
+        contrasena: "",
+        fechaNac: "",
+        fotoPerfil: "",
+
+    },
+    productos: [
+        {
+            marca: "",
+            modelo: "",
+            color: "",
+
+        },
+    ],
+
+};
+module.exports = data
