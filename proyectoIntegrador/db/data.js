824ff84ef9b6256b3794bc21c48cffc7c14934f1
GPreuss <gpreuss@udesa.edu.ar>
2022-04-11 17:36:54 -0300

    linda db

--- proyectoIntegrador/db/data.js
+++ proyectoIntegrador/db/data.js
@@ -10,66 +10,97 @@ let data = {
     },
     productos: [
         {
-            imagen: "",
+            imagen: "/images/products/iphone6.jpg",
             nombre: "Iphone 6",
             descripcion: "Capacidad de 64gb, color Negro",
             fechaCarga: "26/04/2017",
         },
         {
-            imagen: "",
+            imagen: "/images/products/GalaxyS10.jpg",
             nombre: "Samsung Galaxy S10",
             descripcion: "Capacidad de 128gb, color Gris",
             fechaCarga: "27/02/2020",
         },
         {
-            imagen: "",
+            imagen: "/images/products/motoG7.jpg",
             nombre: "Moto G7",
             descripcion: "Capacidad de 64gb, color rojo",
             fechaCarga: "3/10/2019",
         },
         {
-            imagen: "",
+            imagen: "/images/products/NokiaLumia.png",
             nombre: "Nokia Lumia 929",
             descripcion: "Capacidad 64gb, color Blanco",
             fechaCarga: "30/01/2021",
         },
         {
-            imagen: "",
+            imagen: "/images/products/iphone7.jpg",
             nombre: "Iphone 7",
             descripcion: "Capacidad 128gb, color Rosa",
             fechaCarga: "12/04/2020",
         },
         {
-            imagen: "",
+            imagen: "/images/products/galaxyS9.webp",
             nombre: "Samsung Galaxy S9",
             descripcion: "Capacidad 256gb, color Vileta Metalico",
             fechaCarga: "29/12/2020",
         },
         {
-            imagen: "",
+            imagen: "/images/products/Evolve.jfif",
             nombre: "Black Berry Evolve",
             descripcion: "Capacidad 64gb, color Negro",
             fechaCarga: "19/06/2021",
         },
         {
-            imagen: "",
+            imagen: "/images/products/iphoneX.jpg",
             nombre: "Iphone X",
             descripcion: "capacidad 256gb, color Blanco",
             fechaCarga: "08/04/2021",
         },
         {
-            imagen: "",
+            imagen: "/images/products/iphone11.jfif",
             nombre: "Iphone 11",
             descripcion: "Capacidad 64gb, Color Rojo",
             fechaCarga: "15/02/2022",
         },
         {
-            imagen: "",
+            imagen: "/images/products/A72.webp",
             nombre: "Samsung A72",
             descripcion: "Capacidad 128gb, Color Violeta",
             fechaCarga: "19/05/2021",
         },
     ],
-
+    comentarios: [
+        {
+            nombreU:"Pablo Fuentes",
+            texto:"Lo compre hace unos meses y la bateria ya dejo de funcionar.",
+            imgPerfil:"/images/users/default-image.png",
+        },
+        {
+            nombreU:"Juana Gutierrez",
+            texto:"Muy bueno, me encanta la interfaz.",
+            imgPerfil:"/images/users/default-image.png",
+        },
+        {
+            nombreU:"Marcos007",
+            texto:"Muy bueno, me encanta la calidad de sonido y la resolucion de la pantalla.",
+            imgPerfil:"/images/users/default-image.png",
+        },
+        {
+            nombreU:"Pabloco",
+            texto:"Me llego roto, una desgracia. No recomiendo este producto.",
+            imgPerfil:"/images/users/default-image.png",
+        },
+        {
+            nombreU:"Jose Martinez",
+            texto:"No le encontre ningun problema especifico pero no considero que sea tan bueno como otros de su misma gama.",
+            imgPerfil:"/images/users/default-image.png",
+        },
+        {
+            nombreU:"Juan Carlos Messi",
+            texto:"Muy bueno lo recomiendo totalmente para los que quieran un telefono simple pero que ande muy rapido.",
+            imgPerfil:"/images/users/default-image.png",
+        },
+    ],
 };
 module.exports = data
