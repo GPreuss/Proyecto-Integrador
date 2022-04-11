bf256729715ef3de4ea710934c7e72b35d9edb74
GPreuss <gpreuss@udesa.edu.ar>
2022-04-11 17:05:05 -0300

    db

--- proyectoIntegrador/db/data.js
+++ proyectoIntegrador/db/data.js
@@ -10,10 +10,64 @@ let data = {
     },
     productos: [
         {
-            marca: "",
-            modelo: "",
-            color: "",
-
+            imagen: "",
+            nombre: "Iphone 6",
+            descripcion: "Capacidad de 64gb, color Negro",
+            fechaCarga: "26/04/2017",
+        },
+        {
+            imagen: "",
+            nombre: "Samsung Galaxy S10",
+            descripcion: "Capacidad de 128gb, color Gris",
+            fechaCarga: "27/02/2020",
+        },
+        {
+            imagen: "",
+            nombre: "Moto G7",
+            descripcion: "Capacidad de 64gb, color rojo",
+            fechaCarga: "3/10/2019",
+        },
+        {
+            imagen: "",
+            nombre: "Nokia Lumia 929",
+            descripcion: "Capacidad 64gb, color Blanco",
+            fechaCarga: "30/01/2021",
+        },
+        {
+            imagen: "",
+            nombre: "Iphone 7",
+            descripcion: "Capacidad 128gb, color Rosa",
+            fechaCarga: "12/04/2020",
+        },
+        {
+            imagen: "",
+            nombre: "Samsung Galaxy S9",
+            descripcion: "Capacidad 256gb, color Vileta Metalico",
+            fechaCarga: "29/12/2020",
+        },
+        {
+            imagen: "",
+            nombre: "Black Berry Evolve",
+            descripcion: "Capacidad 64gb, color Negro",
+            fechaCarga: "19/06/2021",
+        },
+        {
+            imagen: "",
+            nombre: "Iphone X",
+            descripcion: "capacidad 256gb, color Blanco",
+            fechaCarga: "08/04/2021",
+        },
+        {
+            imagen: "",
+            nombre: "Iphone 11",
+            descripcion: "Capacidad 64gb, Color Rojo",
+            fechaCarga: "15/02/2022",
+        },
+        {
+            imagen: "",
+            nombre: "Samsung A72",
+            descripcion: "Capacidad 128gb, Color Violeta",
+            fechaCarga: "19/05/2021",
         },
     ],
 
