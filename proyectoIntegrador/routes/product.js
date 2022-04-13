30ecc9b12334c50906d9b468170fdc9ead00c07c
GPreuss <gpreuss@udesa.edu.ar>
2022-04-13 16:56:52 -0300

    .

--- proyectoIntegrador/routes/product.js
+++ proyectoIntegrador/routes/product.js
@@ -5,5 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) { 
   res.render('product', { title: 'Express' });
 });
-
+router.get('/add', function(req, res, next) {
+  res.render('product-add', { title: 'Express' });
+});
 module.exports = router;
