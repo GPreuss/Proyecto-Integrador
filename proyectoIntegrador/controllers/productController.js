1183a6af8120d0653949493301158f3a9043de6b
bautistafernandezsasso <bautifernandezs@gmail.com>
2022-06-21 18:21:32 -0300

    product controller

--- proyectoIntegrador/controllers/productController.js
+++ proyectoIntegrador/controllers/productController.js
@@ -2,15 +2,63 @@ const data = require("../db/data")
 const db = require("../database/models")
 const productos = db.Product;
 const users = db.User
+const comments = db.Comentario
+const op = db.Sequelize.Op;//contiene los operadores para usar en metodos de sequelize
+const bcrypt = require('bcryptjs')
 
 const controller = {
     productDetalle: function(req, res, next) { 
         res.render('product', {data: data.productos, comentarios: data.comentarios});      
     },
-    productAdd: function(req, res, next) { 
+    /*productAdd: function(req, res, next) { 
         res.render('product-add', {data: data.usuario});
         //
+    },*/
+
+    show: function (req,res){
+        productos.findOne({
+            where: [{id: req.params.id}],
+            include: [{association: 'publicadorProducto'}, {association: 'comentarios', order: [['createdAt', 'order','DESC']]}],
+            //order: ['comentarios', 'order', 'desc']
+        })
+            .then(function(unProduct){
+                let comentariosOrdenados = unProduct.comentarios.slice().sort((a,b) => b.createdAt - a.createdAt);//como la consigna pide que los comentarios esten en orden descendiente ordenados por createdAt, con esta linea resolvemos esto
+                unProduct.comentarios = comentariosOrdenados;
+                //res.send(comentariosOrdenados)
+
+                let comentadores = [];//checkear si el codigo este esta bien o hay otra forma de resolver
+                if(unProduct.comentarios[0] != undefined){
+                    //return res.send('hay comentarios')
+                    for(let i = 0; i < unProduct.comentarios.length; i++){
+                        users.findOne({
+                            where: [{id: unProduct.comentarios[i].publicador}]
+                        })
+                        .then(function(unComentador){
+                            comentadores.push(unComentador);
+                            if(i == unProduct.comentarios.length - 1){
+                                //return res.send(comentadores)
+                                return res.render('product', {info: unProduct, comentadores: comentadores, id: req.params.id});
+                            }
+                        })
+                    }
+                } else {
+                    //return res.send('no hay comentarios')
+                    return res.render('product', {info: unProduct, comentadores: [], id: req.params.id});
+                }
+                //return res.send(unTelefono)
+                
+            })
+        //return res.render('product', {info: data, array: array, id: req.params.id});
     },
+
+    productAdd: function (req, res) {
+        if (req.session.user == undefined) {
+            return res.redirect('/')
+        } else {
+            return res.render('product-add' , {info: data});
+        };
+    },
+
     store: function(req, res){
         //Obtener los datos del formulario y armar el objeto literal que quiero guardar
         let producto = {
@@ -27,5 +75,6 @@ const controller = {
             })
             .catch( error => console.log(error))
     },
+
 }
 module.exports = controller;
--- proyectoIntegrador/routes/users.js
+++ proyectoIntegrador/routes/users.js
@@ -7,7 +7,7 @@ router.get('/profile', controller.perfil);
 
 router.get('/profile/edit', controller.editarPerfil);
 
-router.get('/register', controller.create);
+/*router.get('/register', controller.create);
 
 router.post('/store', controller.storeUser);
 
@@ -15,6 +15,6 @@ router.get('/login',controller.login);
 
 router.post('/login', controller.signIn);
 
-router.post('/store', controller.storeUser);
+router.post('/store', controller.storeUser);*/
 
 module.exports = router;
