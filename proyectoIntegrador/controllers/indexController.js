1ba7366745d2f78a8abdfd1fbcbcc1e3855a7797
GPreuss <gpreuss@udesa.edu.ar>
2022-06-22 00:20:46 -0300

    ..

--- proyectoIntegrador/controllers/indexController.js
+++ proyectoIntegrador/controllers/indexController.js
@@ -13,6 +13,15 @@ const controller = {
     login: function (req, res, next) {
         res.render('login');
     },
+    /*login: function (req, res) {
+        //mostrar el form de registro
+        //Chequear que un usario esté logueado
+        if (req.session.user != undefined) {
+            return res.redirect('/')
+        } else {
+            return res.render('login');
+        }
+    },*/
     create: function (req, res) {
         console.log(users)
         return res.render('register')
@@ -33,67 +42,7 @@ const controller = {
             })
             .catch(error => console.log(error))
     },
-    /*
-    storeProfile: function (req, res){
-        
-        console.log(req.body) // aca deberia llegar lo que mando el usuario
-
-        var image;
-        if (!(req.file.filename)){
-            console.log('entro al filename')
-            image = '/images/users/default-image.png'
-        } else {
-            image = req.file.filename
-        }
-        if(req.body.password.length<3){
-            res.locals.errores={mensaje:"la contraseña debe tener mas de tres caracteres"}
-            return res.render("register")
-        }
-        let user = { 
-            email: req.body.email,
-            username:req.body.username,
-            password: bcrypt.hashSync(req.body.password, 10),
-            date: req.body.date,
-            dni: req.body.dni,
-            image: req.file.filename
-        }
-
-        users.findOne({
-            where: [{email: user.email}] //encontrar un mail que coincida con el mail que ingreso el usuario
-        })
-
-        .then(function(usuario){ //el then es para que el codigo se ejecute recien cuando se chequee la linea de arriba (el find one)
-            if (usuario) {
-                console.log ('el usuario ya existe')
-                res.locals.errores = {mensaje: 'El email ya esta en uso'}
-                console.log(res.locals.errores)
-                return res.render('register')
-            }else{
-                res.locals.errores = ''; 
-                console.log ('el usuario no existe')
-                users.create(user) //create agarra el objeto, se lo manda a la table en la bd y cuando esta lo guarda, devuelve el registro como parametro de la funcion del then
-                .then(function(respuesta){  //en el parametro recibimos el registro que se acaba de crear en la base de datos
-                    // return res.send(respuesta)
-                    return res.redirect('/index'); //redirigir falta ponerle el id del usuario en cuestion -> session
-                })
-            }
-        })
-
-        
-
-            .catch(error => console.log (error))
-
-    },
-    */
-    login: function (req, res) {
-        //mostrar el form de registro
-        //Chequear que un usario esté logueado
-        if (req.session.user != undefined) {
-            return res.redirect('/')
-        } else {
-            return res.render('login');
-        }
-    },
+    
     logout: function (req, res) {
         //destruir session
         req.session.destroy();
@@ -105,52 +54,23 @@ const controller = {
 
         return res.redirect('/');
     },
-
-    signIn: function (req, res) {
-        console.log("entre al sign in");
+    signIn: function(req,res){
         users.findOne({
-                where: [{
-                    email: req.body.email
-                }]
-            })
-            .then(function (user) {
-                //si trajo un usuario hay que chequear la contraseña con compareSync()
-                //Si las contraseñas no coincuiden mandamos mensaje de error.
-                console.log(req.body)
-                console.log('el usuario es: ' + user);
-
-                if (user) {
-                    console.log('entro al if(user)');
-                    if (bcrypt.compareSync(req.body.password, user.password)) {
-                        //Si el usuario tildó recordarme creo la cookie
-                        if (req.body.remember) {
-                            res.cookie('userId', user.dataValues.id, {
-                                maxAge: 1000 * 60 * 10
-                            });
-                        }
-                        console.log('coinciden');
-                        req.session.user = user.dataValues;
-                        res.locals.errores = ''
-                        console.log('los errores son' + res.locals.errores);
-                        return res.redirect('/profile/' + user.dataValues.id)
-                    } else {
-                        res.locals.errores = {
-                            mensaje: "la password no concide"
-                        };
-                        console.log('los errores son' + res.locals.errores);
-                        return res.render('login')
-                    }
-
-                } else {
-                    res.locals.errores = {
-                        mensaje: "El email es incorrecto"
-                    };
-                    console.log(res.locals.errores);
-                    return res.render('login')
-                }
-            })
-            .catch(error => console.log(error))
-
+            where:[{userName : req.body.usuario}]
+        })
+        console.log()
+        .then(function(users){
+            //falta la validacion si existe o no el mail
+        if(users){
+            req.session.user = users.dataValues ;
+            // si el usuario tildo recordame creo la cookie
+            res.cookie('userID',users.dataValues.id,{maxAge:1000*60*10})
+        }
+        console.log(req.session.user)
+        // console.log(req.session.user); //para ver si existe la session 
+            return res.redirect('/')
+        })
+        .catch(error => console.log(error))
     },
     search: function (req, res, next) {
         res.render('search-results', {
