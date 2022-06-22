8e4bf24bcb1d41b6d7109e67b120940821a879bb
GPreuss <gpreuss@udesa.edu.ar>
2022-06-22 20:32:21 -0300

    imagenes

--- proyectoIntegrador/controllers/productController.js
+++ proyectoIntegrador/controllers/productController.js
@@ -26,7 +26,7 @@ const controller = {
                             association: "productoComentado"
                         }],
                         where: [{
-                            producto: elProducto.id
+                            producto: elProducto
                         }],
                             order: [[['id', 'DESC']]]
                     
@@ -45,58 +45,13 @@ const controller = {
         res.render('product-add', {data: data.usuario});
         //
     },
-
-    /*show: function (req,res){
-        productos.findOne({
-            where: [{id: req.params.id}],
-            include: [{association: 'publicadorProducto'}, {association: 'comentarios', order: [['createdAt', 'order','DESC']]}],
-            //order: ['comentarios', 'order', 'desc']
-        })
-            .then(function(unProduct){
-                let comentariosOrdenados = unProduct.comentarios.slice().sort((a,b) => b.createdAt - a.createdAt);//como la consigna pide que los comentarios esten en orden descendiente ordenados por createdAt, con esta linea resolvemos esto
-                unProduct.comentarios = comentariosOrdenados;
-                //res.send(comentariosOrdenados)
-
-                let comentadores = [];//checkear si el codigo este esta bien o hay otra forma de resolver
-                if(unProduct.comentarios[0] != undefined){
-                    //return res.send('hay comentarios')
-                    for(let i = 0; i < unProduct.comentarios.length; i++){
-                        users.findOne({
-                            where: [{id: unProduct.comentarios[i].publicador}]
-                        })
-                        .then(function(unComentador){
-                            comentadores.push(unComentador);
-                            if(i == unProduct.comentarios.length - 1){
-                                //return res.send(comentadores)
-                                return res.render('product', {info: unProduct, comentadores: comentadores, id: req.params.id});
-                            }
-                        })
-                    }
-                } else {
-                    //return res.send('no hay comentarios')
-                    return res.render('product', {info: unProduct, comentadores: [], id: req.params.id});
-                }
-                //return res.send(unTelefono)
-                
-            })
-        //return res.render('product', {info: data, array: array, id: req.params.id});
-    },
-
-    /*productAdd: function (req, res) {
-        if (req.session.user == undefined) {
-            return res.redirect('/')
-        } else {
-            return res.render('product-add' , {info: data});
-        };
-    },*/
-
     store: function(req, res){
         //Obtener los datos del formulario y armar el objeto literal que quiero guardar
         let producto = {
             productName: req.body.productName,
             descripcion: req.body.descripcion,
             publicador: req.session.user.id,
-            //avatar: req.body.avatar,
+            imagen: req.file.filename,
         }
         //Guardar la info en la base de datos
         productos.create(producto)
--- proyectoIntegrador/routes/product.js
+++ proyectoIntegrador/routes/product.js
@@ -6,19 +6,21 @@ const path = require('path');
 
 var storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, path.join(__dirname, '../public/images/products'));
-    }, 
-    filename: (req, file, cb) => {
-        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-    }
-});
-var upload = multer({storage: storage});
+        cb(null, path.join(__dirname, '../public/images/products'))
+    },
+    filename: (req, file, cb) =>{
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }});
+    
+    var upload = multer({storage: storage})
 
 /* GET home page. */
-router.get('/:id', controller.productDetalle);
+
 
 router.get('/add', controller.productAdd);
 
-router.post('/store', controller.store);
+router.post('/store', upload.single('imagen'), controller.store);
+
+router.get('/detalle/:id', controller.productDetalle);
 
 module.exports = router;
