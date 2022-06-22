a4edd2d592306bf91023ddbd887f3468272352a5
GPreuss <gpreuss@udesa.edu.ar>
2022-06-22 15:21:35 -0300

    .

--- proyectoIntegrador/controllers/indexController.js
+++ proyectoIntegrador/controllers/indexController.js
@@ -33,58 +33,7 @@ const controller = {
             })
             .catch(error => console.log(error))
     },
-    /*
-    storeProfile: function (req, res){
-        
-        console.log(req.body) // aca deberia llegar lo que mando el usuario
-
-        var image;
-        if (!(req.file.filename)){
-            console.log('entro al filename')
-            image = '/images/users/default-image.png'
-        } else {
-            image = req.file.filename
-        }
-        if(req.body.password.length<3){
-            res.locals.errores={mensaje:"la contraseña debe tener mas de tres caracteres"}
-            return res.render("register")
-        }
-        let user = { 
-            email: req.body.email,
-            username:req.body.username,
-            password: bcrypt.hashSync(req.body.password, 10),
-            date: req.body.date,
-            dni: req.body.dni,
-            image: req.file.filename
-        }
-
-        users.findOne({
-            where: [{email: user.email}] //encontrar un mail que coincida con el mail que ingreso el usuario
-        })
-
-        .then(function(usuario){ //el then es para que el codigo se ejecute recien cuando se chequee la linea de arriba (el find one)
-            if (usuario) {
-                console.log ('el usuario ya existe')
-                res.locals.errores = {mensaje: 'El email ya esta en uso'}
-                console.log(res.locals.errores)
-                return res.render('register')
-            }else{
-                res.locals.errores = ''; 
-                console.log ('el usuario no existe')
-                users.create(user) //create agarra el objeto, se lo manda a la table en la bd y cuando esta lo guarda, devuelve el registro como parametro de la funcion del then
-                .then(function(respuesta){  //en el parametro recibimos el registro que se acaba de crear en la base de datos
-                    // return res.send(respuesta)
-                    return res.redirect('/index'); //redirigir falta ponerle el id del usuario en cuestion -> session
-                })
-            }
-        })
-
-        
-
-            .catch(error => console.log (error))
-
-    },
-    */
+    
     login: function (req, res) {
         //mostrar el form de registro
         //Chequear que un usario esté logueado
--- proyectoIntegrador/routes/product.js
+++ proyectoIntegrador/routes/product.js
@@ -1,6 +1,18 @@
 var express = require('express');
 var router = express.Router();
 const controller = require('../controllers/productController');
+const multer = require('multer');
+const path = require('path');
+
+var storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(__dirname, '../public/images/products'));
+    }, 
+    filename: (req, file, cb) => {
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+var upload = multer({storage: storage});
 
 /* GET home page. */
 router.get('/', controller.productDetalle);
