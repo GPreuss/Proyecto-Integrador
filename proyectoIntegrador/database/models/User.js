fdfbbc911062e8aa01d8faf5716a555fc4e25c37
bautistafernandezsasso <bautifernandezs@gmail.com>
2022-06-22 13:19:29 -0300

    model bien

--- proyectoIntegrador/database/models/User.js
+++ proyectoIntegrador/database/models/User.js
@@ -34,7 +34,7 @@ module.exports = function (sequelize, dataTypes){
     let config = {
         tableName : "users",
         timestamps: true, 
-        underscored: true, 
+        underscored: false, 
     };
 
     const User = sequelize.define(alias, cols, config);
